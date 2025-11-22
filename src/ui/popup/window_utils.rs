dce979546362ebc114d94a596b86b19d025ea04b
QP Hou <dave2008713@gmail.com>
2025-11-22 13:59:40 -0800

    fix popup window title bar fill color (#64)

--- src/ui/popup/window_utils.rs
+++ src/ui/popup/window_utils.rs
@@ -1,5 +1,5 @@
 /// Standard margin for popup content
-pub const POPUP_MARGIN: f32 = 10.0;
+pub const POPUP_MARGIN: i8 = 10;
 
 pub fn new_center_popup_window(title: &str) -> egui::Window<'_> {
     egui::Window::new(egui::RichText::from(title).size(14.0))
@@ -15,10 +15,15 @@ pub fn show_center_popup_window<R>(
     ctx: &egui::Context,
     open: &mut bool,
     content: impl FnOnce(&mut egui::Ui) -> R,
-) -> Option<egui::InnerResponse<Option<R>>> {
-    let frame = egui::Frame::window(&ctx.style()).inner_margin(POPUP_MARGIN);
-    new_center_popup_window(title)
-        .frame(frame)
-        .open(open)
-        .show(ctx, content)
+) -> Option<egui::InnerResponse<R>> {
+    let win_resp = new_center_popup_window(title).open(open).show(ctx, |ui| {
+        // NOTE: we are wrapping another frame here instead of setting
+        // window.frame() because there is window.show_dyn overrides header
+        // color with the frame's fill color when frame is not None, making it
+        // impossible to set a different color for the window title bar.
+        egui::Frame::new()
+            .inner_margin(POPUP_MARGIN)
+            .show(ui, |ui| content(ui))
+    });
+    win_resp.and_then(|response| response.inner)
 }
